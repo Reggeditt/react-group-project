6013286f6c4732ff78e6d53341fd769dc6358b93
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-04-24 19:35:10 +0300

    Addition of original logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom';
-import logo from '../assets/spaceTravelers-removebg-preview.png';
+import logo from '../assets/planet.png';
 
 const links = [
   { path: '/', text: 'Rockets', id: 'border' },
