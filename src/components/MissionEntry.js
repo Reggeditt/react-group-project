2b3c2e6e342e46edd62a4d3e2a9955fd3d9817c3
Reggeditt <tkodjosarso@gmail.com>
2023-04-24 23:45:56 +0000

    fixed #6; add conditional rendering of status

--- src/components/MissionEntry.js
+++ src/components/MissionEntry.js
@@ -1,15 +1,43 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { joinMission, leaveMission } from '../redux/missions/missionsSlice';
 
-const MissionEntry = ({ mission }) => (
-  <tr>
-    <td>{JSON.parse(mission).mission_name}</td>
-    <td>{JSON.parse(mission).description}</td>
-    <td>probably not a member yet</td>
-    <td>
-      <button type="button">Join mission</button>
-    </td>
-  </tr>
-);
+const MissionEntry = ({ mission }) => {
+  mission = JSON.parse(mission);
+  const dispatch = useDispatch();
+
+  const membershipStatus = (e) => {
+    const statusID = e.target.id;
+    const status = e.target.innerHTML;
+    if (status === 'Join mission') {
+      dispatch(joinMission(statusID));
+      e.target.innerHTML = 'Leave mission';
+    } else {
+      dispatch(leaveMission(statusID));
+      e.target.innerHTML = 'Join mission';
+    }
+  };
+  return (
+    <tr>
+      <td>{mission.mission_name}</td>
+      <td>{mission.description}</td>
+      <td>
+        {
+          mission.reserved ? 'Active Member' : 'NOT A MEMBER'
+        }
+      </td>
+      <td>
+        <button
+          type="button"
+          id={mission.mission_id}
+          onClick={(e) => membershipStatus(e)}
+        >
+          Join mission
+        </button>
+      </td>
+    </tr>
+  );
+};
 
 MissionEntry.propTypes = {
   mission: PropTypes.string.isRequired,
