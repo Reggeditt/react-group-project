f4d8dc2c67fe1b4d89397ebfb38b3283b173970b
Reggeditt <tkodjosarso@gmail.com>
2023-04-24 19:50:41 +0000

    import fetch, access and render data.

--- src/components/MissionEntry.js
+++ src/components/MissionEntry.js
@@ -1,18 +1,15 @@
 import PropTypes from 'prop-types';
 
-const MissionEntry = ({ mission }) => {
-  console.log(mission);
-  return (
-    <tr>
-      <td>{JSON.parse(mission).mission_name}</td>
-      <td>{JSON.parse(mission).description}</td>
-      <td>probably not a member yet</td>
-      <td>
-        <button type="button">Join mission</button>
-      </td>
-    </tr>
-  );
-};
+const MissionEntry = ({ mission }) => (
+  <tr>
+    <td>{JSON.parse(mission).mission_name}</td>
+    <td>{JSON.parse(mission).description}</td>
+    <td>probably not a member yet</td>
+    <td>
+      <button type="button">Join mission</button>
+    </td>
+  </tr>
+);
 
 MissionEntry.propTypes = {
   mission: PropTypes.string.isRequired,
