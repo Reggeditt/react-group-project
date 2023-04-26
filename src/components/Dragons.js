8b3668fc2fe4fe85f44a86581c79f087c6b5e84b
dieum512 <akonkwadieum222@gmail.com>
2023-04-26 17:49:36 +0200

    Update the Dragon.js file

--- src/components/Dragons.js
+++ src/components/Dragons.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+// import { getDragons } from '../redux/dragons/dragonsSlice';
+import { getDragons } from '../redux/dragon/dragonSlice';
+import DragonEntry from './DragonsEntry';
 
 function Dragons() {
+  const dispatch = useDispatch();
+  const dragonsData = useSelector((store) => store.dragons);
+  const fetch = useRef(true);
+
+  useEffect(() => {
+    if (fetch.current) {
+      if (dragonsData.length === 0) {
+        fetch.current = false;
+        dispatch(getDragons());
+      }
+    }
+  }, [dispatch, dragonsData.length]);
   return (
-    <div>Dragons</div>
+    <div className="dragonBody">
+      {dragonsData.map((dragon) => (
+        <DragonEntry
+          key={dragon.id}
+          name={dragon.name}
+          type={dragon.type}
+          img={dragon.flickr_image}
+          id={dragon.id}
+          reserved={dragon.reserved}
+        />
+      ))}
+    </div>
   );
 }
 
